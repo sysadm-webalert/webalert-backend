f676e14cda3c1a238a516ec6440c5cea0ef5e1b0
Víctor <39667448+VGzsysadm@users.noreply.github.com>
2024-12-28 22:49:17 +0100

    TASK-60 Refactor Threshold validator

--- src/Validator/ThresholdValidator.php
+++ src/Validator/ThresholdValidator.php
@@ -17,39 +17,58 @@ class ThresholdValidator extends ConstraintValidator
             return;
         }
 
-        if ($constraint->type === 'status_code') {
-            $singleCodePattern = '/^\d{3}$/';
-            $rangePattern = '/^\d{3}-\d{3}$/';
+        switch ($constraint->type) {
+            case 'status_code':
+                $this->validateStatusCode($value, $constraint);
+                break;
+            case 'max_response':
+                $this->validateMaxResponse($value, $constraint);
+                break;
+            case 'percent':
+                $this->validatePercent($value, $constraint);
+                break;
+        }
+    }
 
-            if (preg_match($singleCodePattern, $value)) {
-                $number = (int)$value;
-                if ($number >= 100 && $number <= 599) {
-                    return;
-                }
+    private function validateStatusCode($value, Constraint $constraint): void
+    {
+        $singleCodePattern = '/^\d{3}$/';
+        $rangePattern = '/^\d{3}-\d{3}$/';
+
+        if (preg_match($singleCodePattern, $value)) {
+            $number = (int)$value;
+            if ($number >= 100 && $number <= 599) {
+                return;
             }
-
-            if (preg_match($rangePattern, $value)) {
-                [$start, $end] = explode('-', $value);
-                $start = (int)$start;
-                $end = (int)$end;
-
-                if ($start >= 100 && $end <= 599 && $start <= $end) {
-                    return;
-                }
-            }
-
-            $this->context->buildViolation($constraint->invalidStatusCode)
-                ->setParameter('{{ value }}', $value)
-                ->addViolation();
         }
 
-        if ($constraint->type === 'max_response' && (!is_numeric($value) || (float) $value <= 0)) {
+        if (preg_match($rangePattern, $value)) {
+            [$start, $end] = explode('-', $value);
+            $start = (int)$start;
+            $end = (int)$end;
+
+            if ($start >= 100 && $end <= 599 && $start <= $end) {
+                return;
+            }
+        }
+
+        $this->context->buildViolation($constraint->invalidStatusCode)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+    }
+
+    private function validateMaxResponse($value, Constraint $constraint): void
+    {
+        if (!is_numeric($value) || (float) $value <= 0) {
             $this->context->buildViolation($constraint->invalidResponseTime)
                 ->setParameter('{{ value }}', $value)
                 ->addViolation();
         }
-        
-        if ($constraint->type === 'percent' && (!is_numeric($value) || $value < 1 || $value > 99)) {
+    }
+
+    private function validatePercent($value, Constraint $constraint): void
+    {
+        if (!is_numeric($value) || $value < 1 || $value > 99) {
             $this->context->buildViolation($constraint->invalidPercent)
                 ->setParameter('{{ value }}', $value)
                 ->addViolation();
